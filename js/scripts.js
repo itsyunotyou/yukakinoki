a524436c938f266e368626a5b62968a836f40a21
Yu Kakinoki <yukakinoki@gmail.com>
2024-01-29 20:45:45 +0900

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,35 @@
-window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+// Get all table rows within a table
+var tableRows = document.querySelectorAll("table tr");
 
-(function () {
-  "use strict";
+// Attach a click event listener to each table row
+tableRows.forEach(function(row) {
+  row.addEventListener("click", function() {
+    // Get the value of the 'href' attribute of the clicked row
+    var hrefValue = this.getAttribute("data-tab");
 
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
+    // Get the element with the corresponding id
+    var targetElement = document.getElementById(hrefValue);
 
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
+    // Hide siblings and fade in the target element
+    Array.from(targetElement.parentNode.children).forEach(function(sibling) {
+      if (sibling !== targetElement) {
+        sibling.style.display = "none";
       }
-    }
-  }
+    });
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-})();
+    targetElement.style.display = "flex"; // Assuming the target element should be displayed
+    // Alternatively, you can use other methods to show the element, like adding a CSS class.
+    // targetElement.classList.add("visible");
 
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+    // If you want a fadeIn effect, you can use CSS transitions or JavaScript animations
+    // For simplicity, I'm using a simple fade-in effect with opacity
+    targetElement.style.opacity = 0;
+    var fadeInInterval = setInterval(function() {
+      if (targetElement.style.opacity < 1) {
+        targetElement.style.opacity = parseFloat(targetElement.style.opacity) + 0.1;
+      } else {
+        clearInterval(fadeInInterval);
+      }
+    }, 100);
+  });
+});
